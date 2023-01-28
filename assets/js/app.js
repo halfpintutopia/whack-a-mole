fc303ae4db2cf5588e9ed31ef79cce05938df1df
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-01-28 17:53:39 +0100

    Add logic to randomly select square where mole is to appear

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 const grid = document.getElementById('grid');
+const score = document.getElementById('score');
+const timer = document.getElementById('timer');
+let squares,
+    mole,
+    currentScore = 0,
+    currentTimer = 60;
+
 const columns = 3;
 const rows = 3;
 
@@ -11,6 +18,24 @@ function createGameBoard() {
         grid.append(square);
         i++;
     }
+    score.innerHTML = currentScore;
+    timer.innerHTML = currentTimer;
+    squares = document.querySelectorAll('.square');
 }
 
 createGameBoard();
+
+function randomSquare() {
+    [...squares].map(square => {
+        square.classList.remove('mole');
+    });
+
+    squares[Math.floor(Math.random() * squares.length)].classList.add('mole');
+}
+
+function moveMole() {
+    let timerID = null;
+    timerID = setInterval(randomSquare, 500)
+}
+
+moveMole()
